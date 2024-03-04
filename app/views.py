a9599620e844036db8a71e895f1117fd347f5296
Sviatlana Yermalinskaya <orimailrb@gmail.com>
2024-03-04 19:14:16 +0300

    Add base profile.html

--- app/views.py
+++ app/views.py
@@ -1,15 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 
+isAuth = False
 
-# Create your views here.
+@csrf_exempt
 def index(request):
+    global isAuth
+    if isAuth and request.method == "GET":
+        return render(request, 'profile.html')
+    if request.method == "POST" and isAuth:
+        isAuth = False
     return render(request, 'index.html')
 
 @csrf_exempt
 def register(request):
+
     return render(request, 'registration.html', {'subtitle': ': Регистрация'})
 
 @csrf_exempt
 def login(request):
+    if request.method == 'POST':
+        global isAuth
+        isAuth = True
+        return redirect('/')
     return render(request, 'login.html', {'subtitle': ': Авторизация'})
