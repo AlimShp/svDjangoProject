4153c5bddc2615cf43fc73ce23e24acddd2265b1
Sviatlana Yermalinskaya <orimailrb@gmail.com>
2024-03-05 00:03:39 +0300

    add session logic

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class ModelReg(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    user_session = models.CharField(max_length=48)
--- app/views.py
+++ app/views.py
@@ -1,26 +1,75 @@
+import random
+
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 
-isAuth = False
+from app.models import ModelReg
+
 
 @csrf_exempt
 def index(request):
-    global isAuth
-    if isAuth and request.method == "GET":
-        return render(request, 'profile.html')
-    if request.method == "POST" and isAuth:
-        isAuth = False
+    user_session = request.COOKIES.get('user_session')
+    if user_session:
+        print('Есть куки: ' + user_session)
+        users = ModelReg.objects.all()
+        for u in users:
+            if u.user_session == user_session:
+                print(request.method)
+                if request.method == "GET":
+                    return render(request, 'profile.html', {'user': u.name})
+                if request.method == "POST":
+                    print('Пользователь вышел')
+                    html = render(request, 'index.html')
+                    html.delete_cookie('user_session')
+                    return html
+        else:
+            print('Куки удалены')
+            html = render(request, 'index.html')
+            html.delete_cookie('user_session')
+            return html
+    print('Нет куки')
     return render(request, 'index.html')
+
 
 @csrf_exempt
 def register(request):
-
+    if request.method == 'POST':
+        users = ModelReg.objects.all()
+        for u in users:
+            if request.POST['email'] == u.email:
+                return render(request, 'registration.html', {'subtitle': ': Регистрация', 'form': f'Пользователь с почтой {u.email} уже зарегистрирован!'})
+        reg = ModelReg()
+        reg.email = request.POST['email']
+        reg.password = request.POST['pass']
+        reg.name = request.POST['name']
+        reg.user_session = generate_session(reg.email)
+        reg.save()
+        html = redirect('/')
+        html.set_cookie('user_session', reg.user_session)
+        return html
     return render(request, 'registration.html', {'subtitle': ': Регистрация'})
+
 
 @csrf_exempt
 def login(request):
+    answer = ''
     if request.method == 'POST':
-        global isAuth
-        isAuth = True
-        return redirect('/')
-    return render(request, 'login.html', {'subtitle': ': Авторизация'})
+        users = ModelReg.objects.all()
+        for u in users:
+            if request.POST['email'] == u.email:
+                if request.POST['pass'] == u.password:
+                    html = redirect('/')
+                    html.set_cookie('user_session', u.user_session)
+                    return html
+                else:
+                    answer = 'Ошибка при вводе пароля!'
+            else:
+                answer = 'Неверное имя пользователя!'
+    return render(request, 'login.html', {'subtitle': ': Авторизация', 'form': answer})
+
+def generate_session(key: str):
+    key = str(abs(key.__hash__()))
+    key += '_'
+    while len(key) < 48:
+        key += chr(random.randint(65, 90))
+    return key
